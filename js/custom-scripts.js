b048d77dfdc9fd452969ef80988bdac5ec13a081
bIropka <BrDm_100>
2016-08-20 15:00:02 +0300

    v 0.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -7,19 +7,23 @@ $(document).ready(function () {
     /*** end of initial scripts **/
 
 
-    /** scripts for adaptive **
+    /** scripts for adaptive **/
 
     if ($(window).width() < '981'){
-
+        $('nav ul').css('display', 'none');
+        $('.header-menu ul').css('display', 'none');
     } else {
-
+        $('nav ul').css('display', 'block');
+        $('.header-menu ul').css('display', 'block');
     }
 
     $(window).resize(function(){
         if ($(window).width() < '981'){
-
+            $('nav ul').css('display', 'none');
+            $('.header-menu ul').css('display', 'none');
         } else {
-
+            $('nav ul').css('display', 'block');
+            $('.header-menu ul').css('display', 'block');
         }
     });
 
@@ -75,4 +79,17 @@ $(document).ready(function () {
         $(this).parents('form').find('.form-field:invalid').addClass('invalid-field');
         $(this).parents('form').find('.form-field:valid').addClass('valid-field');
     });
+
+    $('.custom-select .current-value').click(function() {
+        $(this).parent().toggleClass('active');
+    });
+
+    $('.custom-select ul li').click(function() {
+        var currentValue = $(this).html();
+        $(this).parents('.custom-select').find('input').attr('value', currentValue);
+        $(this).parents('.custom-select').find('.current-value').html(currentValue);
+        $(this).parents('.custom-select').removeClass('active');
+    });
+
+
 });
